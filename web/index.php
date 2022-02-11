f42efb0398c2b24cc18e1ae35264103076b41edd
darkwhitefeather <darkwhitefeather@hotmail.com>
2022-02-11 13:42:29 +0800

    修改輪播圖，引入AOS.js

--- web/index.php
+++ web/index.php
@@ -4,16 +4,18 @@
 		<meta charset="UTF-8">
 		<title>曙光教育事業機構</title>
 		<?php include("_link_script.html");?>
-
-		<style id="css">
-			/* 測試用，隨時可刪 */
-			.carousel__slide{
-				display: flex;
-				flex-direction: column;
-				align-items: center;
+		<style>
+			.carousel-caption{
+				/* 敘述文字變黑 */
+				color: black;
+			}
+			.banner{
+				border:1px #dae0e5 solid;
+				box-shadow: 1px ​1px 6px black , -1px -1px 1px #dae0e5;
 			}
 
 		</style>
+		
 	</head>
 	<body class="home">
 		<?php include("_header.html");?>
@@ -26,13 +28,52 @@
 			</div> -->
 			
 			<!-- Banner -->
-			<div class="banner img-fluid">
-				<img src="image/5_2.jpg" alt="">
-				<!-- <p class="test">輪播圖位置圖</p> -->
-				<!-- <a href="javascript:void(0)">
-					<img src="image/01.png" alt="Banner輪播廣告圖">
-				</a> -->
+			<div id="bannerCarousel" class="carousel slide banner" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#bannerCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#bannerCarousel" data-slide-to="1"></li>
+					<li data-target="#bannerCarousel" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img src="image/Banner01.jpg" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>曙光頭版-我是抬頭01</h5>
+							<p>會員價食品不能下載坐在真實，寫作他人辦公本地領域較大，我說黑暗出版社千萬綜合減少水溝那個活力全球行業網際快車，觀念充滿我這十二時尚賺錢推出達到內部走勢一遍強烈精靈，信用都沒感謝浪漫富岡危機博士網絡我也前後，求購當初儘量消除內容簡介只好優秀所在系統或許，同。</p>
+							<p></p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img src="https://picsum.photos/1000/400?random=1" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>曙光頭版-我是抬頭02</h5>
+							<p>走到願意獲取分類熱情提示訊息，經濟發展，一塊確認查看成立天空忘記來源無線有所每日妹妹兩人和平兒童而言，大型分享日韓紅色，以為會有無派成了幾個出現在擔任難得小姐開通徹底，時期當你一生，森林報名這位屬於回去事實無聊禁止你沒有愛情傢伙戀愛，都會下列對了四周交通。</p>
+							<p></p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img src="https://picsum.photos/1000/400?random=2" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>曙光頭版-我是抬頭03</h5>
+							<p>網遊運行面積認真推薦，特色一系列各種保密損失允許事實變得清晰教材，作者手裡一步簡歷出現考察，留言板足夠搜尋農村附近起來就在人大，一口無論，從而可以用，容易樂隊不說之下凱文這些反覆家庭觀察重要敵人，文化模糊好看不大平衡，開通協議明年關鍵字是從有意明天，排名。</p>
+							<p></p>
+						</div>
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#bannerCarousel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#bannerCarousel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
 			</div>
+
+			<!-- <div id="bannerCarousel" class="banner img-fluid">
+				<div class="carousel__slide"><img src="image/Banner01.jpg" alt="Banner輪播廣告圖01"></div>
+				<div class="carousel__slide"><img src="image/5_2.jpg" alt="Banner輪播廣告圖02"></div>
+			</div> -->
 
 			<!-- 曙光據點、服務成果(人數) -->
 			<div class="digital row padding_3">
@@ -89,8 +130,8 @@
 			</div>
 
 			<!-- 榜單專區 -->
-			<div class="title"> 榜單專區 </div>
-			<div id="honorRollsCarousel" class="carousel">
+			<div class="title" data-aos="fade-up"> 榜單專區 </div>
+			<div id="honorRollsCarousel" class="carousel" data-aos="fade-up">
 				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/001.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="http://www.xn--54q983c.tw/_testWeb/web/image/list/m/m_001.jpg" alt=""></a>
 				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/002.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="http://www.xn--54q983c.tw/_testWeb/web/image/list/m/m_002.jpg" alt=""></a>
 				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/003.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="http://www.xn--54q983c.tw/_testWeb/web/image/list/m/m_003.jpg" alt=""></a>
@@ -113,12 +154,11 @@
 				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/020.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="http://www.xn--54q983c.tw/_testWeb/web/image/list/m/m_020.jpg" alt=""></a>
 				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/021.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="http://www.xn--54q983c.tw/_testWeb/web/image/list/m/m_021.jpg" alt=""></a>
 				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/022.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="http://www.xn--54q983c.tw/_testWeb/web/image/list/m/m_022.jpg" alt=""></a>
-				<a href="http://www.xn--54q983c.tw/_testWeb/web/image/list/023.jpg" class="carousel__slide" data-fancybox="indexHonorRolls"><img src="image/list/m/m_023.jpg" alt=""></a>
 			</div>
 
 			<!-- 影音導覽 -->
-			<div class="title"> 影音導覽 </div>
-			<div id="videoCarousel" class="carousel">
+			<div class="title" data-aos="fade-up"> 影音導覽 </div>
+			<div id="videoCarousel" class="carousel" data-aos="fade-up">
 				<a class="carousel__slide test" data-fancybox="video-gallery" data-src="https://www.youtube.com/embed/GzbOG4MU140"><img src="https://i.ytimg.com/vi/GzbOG4MU140/hqdefault.jpg"/><div >曙光K館</div></a>
 				<a class="carousel__slide test" data-fancybox="video-gallery" data-src="https://www.youtube.com/embed/useWdrICTno"><img src="https://i.ytimg.com/vi/useWdrICTno/hqdefault.jpg"/><div >李揚國文</div></a>
 				<a class="carousel__slide test" data-fancybox="video-gallery" data-src="https://www.youtube.com/embed/1fPRZyd0uu4"><img src="https://i.ytimg.com/vi/1fPRZyd0uu4/hqdefault.jpg"/><div >曙光教育機構_從心出發</div></a>
@@ -135,5 +175,8 @@
 
 		</div>
 		<?php include("_footer.html");?>
+		<script>
+			AOS.init();
+		</script>
 	</body>
 </html>
--- web/js/myJavascript.js
+++ web/js/myJavascript.js
@@ -2,21 +2,29 @@ $(function() {
     //Bootstrap的submenu外掛
     $('[data-submenu]').submenupicker();
 
+    //控制Banner輪播圖
+    $('#bannerCarousel').carousel({
+        interval: 5000
+
+    });
+
     //控制榜單輪播圖
     const honorRollsCarousel=new Carousel(document.querySelector("#honorRollsCarousel"), {
         Dots: true,
+        slidesPerPage:1,
         Autoplay: {
             timeout: 1000,
-            hoverPause: true
+            hoverPause: true,
         }
     });
 
     //控制影音輪播圖
     const videoCarousel=new Carousel(document.querySelector("#videoCarousel"), {
         Dots: true,
+        slidesPerPage:1,
         Autoplay: {
             timeout: 1200,
-            hoverPause: true
+            hoverPause: true,
         }
     });
 
